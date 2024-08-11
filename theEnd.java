00b74fe491e7c252511ef5527e9a5be6608d89fa
kartik-123-hue <72431395+kartik-123-hue@users.noreply.github.com>
2024-08-11 13:12:30 +0530

    Create theEnd.java

---
+++ theEnd.java
@@ -0,0 +1,13 @@
+/*Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+
+
+theEnd("Hello", true) → "H"
+theEnd("Hello", false) → "o"
+theEnd("oh", true) → "o" */
+
+public String theEnd(String str, boolean front) {
+  if(front){
+    return String.valueOf(str.charAt(0));
+  }
+  return String.valueOf(str.charAt(str.length()-1));
+}
